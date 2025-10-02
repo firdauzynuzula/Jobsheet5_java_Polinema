2e65821942085310d9cfe729980a0bb16c0c7d1f
firdauzy <nuzulafirdhauzy@gmail.com>
2025-10-02 12:24:16 +0700

    percobaan 1

--- ifCetakKRS11.java
+++ ifCetakKRS11.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class ifCetakKRS11 {
 public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    System.out.println("Cetak KRS Siakad");
+    System.out.println("-----Cetak KRS Siakad-----");
     System.out.println("Apakah UKT sudah lunas? (True/False): ");
     boolean uktLunas = sc.nextBoolean();
 
