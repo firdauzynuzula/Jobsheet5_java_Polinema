b66cd30d00fe41c8f9148750d46efc56a155cc11
firdauzy <nuzulafirdhauzy@gmail.com>
2025-10-02 12:26:14 +0700

    Tugas 1

--- ifCetakKRS11.java
+++ ifCetakKRS11.java
@@ -6,12 +6,8 @@ public static void main(String[] args) {
     System.out.println("Apakah UKT sudah lunas? (True/False): ");
     boolean uktLunas = sc.nextBoolean();
 
-    if (uktLunas) {
-        System.out.println("Pembayaran UKT terverifikasi");
-        System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
-    } else {
-        System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
-    }
+    String Hasil = uktLunas ? "Pembayaran UKT terverifikasi \nSilahkan cetak KRS dan minta tanda tangan DPA" : "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu";
+    System.out.println(Hasil);
         sc.close();
     }
 }
